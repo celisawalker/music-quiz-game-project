5f001bde7286df378fd0465518bd6b3cb7a09b97
celisawalker <celisa.walker@gmail.com>
2025-02-24 13:41:56 -0800

    created music quiz files and folders; added pseudocode and boilerplate html

---
+++ js/app.js
@@ -0,0 +1,37 @@
+//Game Choice: Music-Themed Quiz
+//Objective: 
+    // Player attempts to answer 10 questions based on music trivia. This game will be on the style of a Buzzfeed quiz but
+        //if the player gets three questions wrong, the game ends
+//Game: 
+    //Create a list of 10 music trivia questions to be stored in an array of objects
+        //The questions presented to the player will be a mix of true/false and multiple choice
+    //present one question at a time to the player - if question is answered correctly, the next question appears
+        //If the question is answered incorrectly, one of the player's three "lives" is removed
+            //Include sound effects for when the player correctly answers a question, when they incorrectly answer a question and when they win or lose the game
+            //Create a counter that starts at three and decrements by one when a question is answered incorrectly
+    //Create a score counter that tallies the player's score - they receive 10 points for each question that is answered correctly
+        //If the player makes it to the end of the game, present them with a congratulatory message and their final score
+    //Include a replay button at the end of the game that appears regardless of whether the player wins or loses
+
+
+//SKILLS TO-DO LIST
+//learn how to add audio to browser
+//have user choose between multiple answers
+//how to add photos to game
+
+/*-------------- Constants -------------*/
+const questions = []
+
+/*---------- Variables (state) ---------*/
+let score = 0;
+let lives = 0;
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
+
+
+/*----------- Event Listeners ----------*/
+document.getElementById("play-btn").addEventListener("click", )
+document.getElementById("play-again-btn").addEventListener("click", )
